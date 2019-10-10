a2b56ad3a141781c651527b58df61efa1b5f1800
anusha <anusha.karkera@juegostudio.net>
2019-10-10 10:49:23 +0530

    undefined type

--- scripts.js
+++ scripts.js
@@ -3,15 +3,25 @@
 
 
 
-var name="anu";
-console.log(name);
-var lastName="sha"
-console.log(lastName)
+// var name="anu";
+// console.log(name);
+// var lastName="sha"
+// console.log(lastName)
+// var age=22;
+// console.log(age);
+// var flag=true;
+// console.log(flag)
+
+
+
+// var name="anu";
 var age=22;
-console.log(age);
-var flag=true;
-console.log(flag)
-
-
-
+console.log(name + age)
+//main note :---
+console.log(age + age);
+//outputs:--- 44 (not 22 22)
+var job,married;
+console.log(job);//undefined
+job = 'teacher';
+married = false;
 

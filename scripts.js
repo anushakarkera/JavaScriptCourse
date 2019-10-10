7d4be005403178e4f35e48a8b4ccfbb659cab980
anusha <anusha.karkera@juegostudio.net>
2019-10-10 10:47:32 +0530

    using var

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
-console.log("diffrent file");
+// console.log("diffrent file");
 
 
 
+
+var name="anu";
+console.log(name);
+var lastName="sha"
+console.log(lastName)
+var age=22;
+console.log(age);
+var flag=true;
+console.log(flag)
 
 
 

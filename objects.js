aa385b9e74b37db8ef88ac5a82aa94044b88619c
anusha <anusha.karkera@juegostudio.net>
2019-10-10 15:30:28 +0530

    object methods and using this operator

--- objects.js
+++ objects.js
@@ -41,3 +41,60 @@ details.age=100;
 console.log(details['age']);
 
 console.log(details)
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Objects and methods
+
+
+
+var details =
+{
+    name:"anusha",
+    age: 22,
+    dob:"17/06/1997",
+    isMarried: false,
+    qualification: "engineer",
+    job:"developer",
+    family : ['ramya','ravindra','shailaja'],
+    calculateAge: function yearOfBirth(year){
+        return 2019-year;
+    }
+};
+
+
+console.log(details);
+console.log(details.family);
+console.log(details.family[1]);
+console.log(details.calculateAge(1997));
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//using this
+
+
+var details1 =
+{
+    name:"anusha",
+    dob:"17/06/1997",
+    isMarried: false,
+    qualification: "engineer",
+    job:"developer",
+    yearOfBirth:1997,
+    family : ['ramya','ravindra','shailaja'],
+    calculateAge: function yearOfBirth(){
+        return 2019-this.yearOfBirth;
+    }
+};
+
+
+console.log(details1.calculateAge());
+
+var age = details1.calculateAge();
+details1.age = age;
+
+console.log(details1)

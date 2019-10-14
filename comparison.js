050ce94b81d7053c560818eaa7a8b650a28aea39
anusha <anusha.karkera@juegostudio.net>
2019-10-14 10:51:53 +0530

    comparision of ts and js, hoisting in js

---
+++ comparison.js
@@ -0,0 +1,43 @@
+//**********************************************************************************************************************
+//js doesnot support optional parameters
+
+
+// function add(a,b?)
+// {
+
+// }
+
+
+
+
+//********************************************************************************************************************
+//below code gives run time error
+// let b=5;
+// b="anusha";
+
+
+
+
+//*********************************************************************************************************************
+
+//js is scripting lang it does not support interface(oops concepts)
+// interface person{
+//     name:String,
+//     id:123
+// }
+
+
+//*********************************************************************************************************************
+//js does not support enum
+
+
+// enum color{
+//     red,green,blue
+// };
+
+
+//below code is grouped in a function to use enum const
+// const colorred = 0;
+// const colorgreen = 1;
+// const colorblue = 2;
+
---
+++ hoisting.js
@@ -0,0 +1,36 @@
+
+
+
+//calculateAge(1998); // valid 
+
+
+
+function calculateAge(year)
+{
+    console.log(2019-year);
+}
+
+calculateAge(1998); // valid even if this is called before function definition.
+
+
+retirement(1990); //does not work
+
+var retirement = function(year){
+    console.log(65 - (2016 - year))
+}
+
+retirement(1990);
+
+console.log(age);
+var age = 23;
+
+
+function foo(){
+    var age = 65;
+    console.log(age);
+}
+
+
+foo();
+console.log(age);
+

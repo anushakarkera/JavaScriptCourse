654559aa607f413b4456390b7ffba5ebcc90f7c5
unknown <anusha.karkera@juegostudio.net>
2019-10-16 12:55:20 +0530

    bind, call, apply

--- Objects and Constructors/closure.js
+++ Objects and Constructors/closure.js
@@ -46,3 +46,63 @@ interviewQuestion('teacher')('john');
 
 
 //*****************************************************************************************************
+//Bind , Call , Apply
+
+var john = {
+    name : 'John',
+    age : 20,
+    job : 'teacher',
+    presentation : function(style,timeOfDay){
+        if(style === 'formal'){
+            console.log('Good '+ timeOfDay + ', Ladies and Gentle men ! I\'m '+ this.name + ', i\'m a ' + this.job + 'and I\'M '+this.age + 'years old.');
+        }else if(style === 'friendly'){
+            console.log('Hey: What\'s up? I\'m '+ this.name + ', i\'m a ' + this.job + 'and I\'M '+this.age + 'years old. Have a nice ' + timeOfDay + '.')
+        }
+    }
+}
+
+
+var emily = {
+    name : 'Emily',
+    age : 29,
+    job : 'designer'
+};
+
+john.presentation('formal','morning');
+//call method takes this as its first arg
+john.presentation.call(emily,'friendly','afternoon')//sets this in presentaion function to emiley object.
+// john.presentation.apply(emily,['friendly','afternoon'])
+var johnFriendly = john.presentation.bind(john,'friendly');
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily,'formal');
+emilyFormal('afternoon');
+
+
+
+
+var years = [1973,1897,1837,2908,2097];
+function arrayCalc(arr , fn)
+{
+    var arrRes = [];
+    for(var i=0;i<arr.length;i++)
+    {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el){
+    return 2016 - el;
+}
+
+
+function isFullAge(limit, el){
+    return (el >= limit);
+}
+
+var age = arrayCalc(years,calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(ages);
+console.log(fullJapan);

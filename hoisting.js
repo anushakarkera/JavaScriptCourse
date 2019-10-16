5a17e974fc604aeb819dfd9deb80eb5186893f25
unknown <anusha.karkera@juegostudio.net>
2019-10-16 10:55:31 +0530

    hoisting

--- hoisting.js
+++ hoisting.js
@@ -12,8 +12,9 @@ function calculateAge(year)
 
 calculateAge(1998); // valid even if this is called before function definition.
 
-
+//does not work for function expressions
 retirement(1990); //does not work
+
 
 var retirement = function(year){
     console.log(65 - (2016 - year))
